ebcccfd677af541dea26736bcd39cfe51e1253a1
Bi Xiaosong <bixiaosong@gmail.com>
2023-12-12 22:13:18 +0100

    added component Header and Banner

---
+++ .history/src/App_20231212221145.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Header from './Header/Header'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Banner />
+    </div>
+  )
+}
+
+export default App
---
+++ .history/src/Banner/Banner_20231212221002.js
@@ -0,0 +1,16 @@
+//Banner.js
+import React from 'react'
+import './Banner.scss'
+import bannerImage from '../assets/img/banner.png'
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner__content'>
+        <h1 className='banner__title'>Chez vous, partout et ailleurs</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
