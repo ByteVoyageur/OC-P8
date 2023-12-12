01388bf9f2290e18a4d391b6086a7ca6de50fbc4
Bi Xiaosong <bixiaosong@gmail.com>
2023-12-12 22:17:12 +0100

    added component Footer but color of logo non changed

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+// Footer.js
+import React from 'react'
+import './Footer.scss'
+import { ReactComponent as Logo } from '../assets/img/logo_kasa.svg'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer__content'>
+        <Logo alt='Kasa Logo' className='footer__logo' />{' '}
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
